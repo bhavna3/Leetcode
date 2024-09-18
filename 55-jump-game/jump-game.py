8e1bd1dc646d33a337de795ee4871b4595505c91
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2024-09-18 18:13:46 +0530

    Time: 344 ms (83.19%) | Memory: 17.8 MB (67.10%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,11 +1,13 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        gas = 0
-        for n in nums:
-            if gas < 0:
-                return False
-            elif n > gas:
-                gas = n
-            gas -= 1
-            
-        return True        
+        #original destination is last index
+        goal = len(nums)-1
+
+        #check backwards
+        for i in range(len(nums)-2,-1,-1):
+            #if we can jump then update
+            if i+nums[i] >= goal:
+                goal = i
+
+        #check if we can reach from first index
+        return True if goal == 0 else False #return goal == 0     
