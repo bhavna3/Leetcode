235d16cac29cc8da60a2feb63ad7ca699ab22235
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2024-11-04 18:10:36 +0530

    Time: 31 ms (34.72%) | Memory: 17.2 MB (27.17%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -2,8 +2,7 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         if n == 1:
             return [["Q"]]
-        if n == 2 or n == 3:
-            return []
+        
 
         results = []
         solution = [-1] * n
