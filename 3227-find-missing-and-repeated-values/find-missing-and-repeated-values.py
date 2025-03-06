b949cd13353ca108615eb86896bd27c7288d5f05
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2025-03-06 15:42:07 +0530

    Time: 8 ms (73.39%) | Memory: 12.6 MB (88.89%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def findMissingAndRepeatedValues(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[int]
+        """
+        count = [1] + [0] * len(grid)**2  
+        for row in grid:
+            for num in row:
+                count[num] += 1
+        return [count.index(2), count.index(0)]
+
+        
