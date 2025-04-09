a106429548f92657bde1d29834601d2c7c33f463
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2025-04-09 15:13:34 +0530

    Time: 8 ms (32.51%) | Memory: 65.4 MB (21.94%) - LeetSync

---
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minOperations = function(nums) {
+    let n = nums.length, k = 0;
+
+    for (let i = 0; i <= n - 3; i++) {
+        if (nums[i] === 0) {
+            nums[i] ^= 1;
+            nums[i + 1] ^= 1;
+            nums[i + 2] ^= 1;
+            k++;
+        }
+    }
+
+    return nums.includes(0) ? -1 : k;
+};
