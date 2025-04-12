2a878e442f319c202901e9120265f42b2e79119e
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2025-04-12 21:59:08 +0530

    Time: 55 ms (77.64%) | Memory: 34.6 MB (34.25%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.py
@@ -1,15 +1,26 @@
-class Solution(object):
-    def minimumIndex(self, nums):
-        mpp, left = {}, {}
-        for i in nums:
-            mpp[i] = mpp.get(i, 0) + 1
-        
-        for i in range(len(nums) - 1):
-            left[nums[i]] = left.get(nums[i], 0) + 1
-            mpp[nums[i]] -= 1
+class Solution:
+    def minimumIndex(self, nums: List[int]) -> int:
+    
+        from collections import Counter
 
-            if left[nums[i]] * 2 > (i + 1) and mpp[nums[i]] * 2 > (len(nums) - i - 1):
+        freq = Counter(nums)
+        n = len(nums)
+        dom, count = 0, 0
+
+        for num, c in freq.items():
+            if c > n // 2:
+                dom, count = num, c
+                break
+
+        left_count = 0
+        for i in range(n - 1):
+            if nums[i] == dom:
+                left_count += 1
+            left_size = i + 1
+            right_size = n - left_size
+            right_count = count - left_count
+
+            if left_count > left_size // 2 and right_count > right_size // 2:
                 return i
-        return -1
 
-        
+        return -1
