9f73c6fddb0b13bc6a2e3f3f9ef2b3718a1be138
bhavna3 <113972221+bhavna3@users.noreply.github.com>
2025-04-27 16:06:04 +0530

    Time: 1331 ms (14.22%) | Memory: 23.9 MB (60.37%) - LeetSync

--- 923-super-egg-drop/super-egg-drop.py
+++ 923-super-egg-drop/super-egg-drop.py
@@ -1,11 +1,29 @@
 class Solution:
     def superEggDrop(self, k: int, n: int) -> int:
-        D1 = [0] * (k + 1)
-        for d in range(1, n + 1):
-            D0 = D1
-            D1 = [0] * (k + 1)
-            for i in range(1, k + 1):
-                D1[i] = D0[i] + D0[i - 1] + 1
-            if D1[-1] >= n:
-                return d
-        return -1       
+        t = [[-1 for p in range(0,n+1)]
+            for q in range(0,k+1)]        
+        return self.solve(k,n,t)
+    
+    def solve(self,k,n,t):
+        if k == 1 or n == 1 or n == 0:
+            return n
+        
+        if t[k][n] != -1:
+            return t[k][n]
+        ans = float('inf')
+        low = 0
+        high = n
+        while low <= high:
+            mid = (low + high) // 2
+            left = self.solve(k-1,mid-1,t)
+            right = self.solve(k,n-mid,t)
+            temp = 1 + max(left,right)
+            
+            if left < right:
+                low = mid+1
+            else:
+                high = mid-1
+                
+            ans = min(ans,temp)
+            t[k][n] = ans
+        return ans
