502feee8f5dd5041e5c618633d7200e3f5d85234
bhavna3 <bhavanamothkur2003@gmail.com>
2025-06-13 20:49:49 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (86.41%) - LeetSync

---
+++ 405-convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.py
@@ -0,0 +1,8 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        if num == 0:
+            return "0"
+        if num < 0:
+            num += 2 ** 32 
+        ans = hex(num)[2:]
+        return ans
