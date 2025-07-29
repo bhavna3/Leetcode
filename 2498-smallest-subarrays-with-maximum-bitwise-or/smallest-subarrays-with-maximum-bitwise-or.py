826f1362912ff7a1e55d8cb7909c947012a3e2e6
bhavna3 <bhavanamothkur2003@gmail.com>
2025-07-29 19:45:45 +0530

    Time: 1017 ms (32.28%) | Memory: 28.9 MB (86.61%) - LeetSync

---
+++ 2498-smallest-subarrays-with-maximum-bitwise-or/smallest-subarrays-with-maximum-bitwise-or.py
@@ -0,0 +1,18 @@
+class Solution:
+    def smallestSubarrays(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [0] * n
+        latest = [-1] * 32
+
+        for i in range(n - 1, -1, -1):
+            far = i 
+
+            for b in range(32):
+                if (nums[i] >> b) & 1:
+                    latest[b] = i
+
+                if latest[b] != -1:
+                    far = max(far, latest[b])
+            result[i] = far - i + 1
+            
+        return result 
