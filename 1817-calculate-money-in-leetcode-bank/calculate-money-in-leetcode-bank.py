441396f5d5544be06a2f9608973896b5fc3ca7a2
bhavna3 <bhavanamothkur2003@gmail.com>
2025-10-26 12:02:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (37.16%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -3,16 +3,13 @@ class Solution:
         week=n//7 #full weeks = monday value
         rem_days=n%7
 
-        total=28*week + 7*(week*(week-1))//2  # 28 per week fixed + additional 7 increase every week
-        new_monday=week+1
+        return 28*week + 7*(week*(week-1))//2  + rem_days*(2*(week+1)+(rem_days-1))//2 
+        #arithmetic progression formula
 
-        for i in range(1,rem_days+1): #O(6) 
-            total+=new_monday+(i-1)             # full week (7days) + rem days (b/w 1-6)
-        return total
-
-        #better 
-        #TC O(6 or 7)=O(1)
+        #Optimal 
+        #TC O(1) 
         #SC O(1)
+
 
 
 
