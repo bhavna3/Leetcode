efda1cae5a49d7cf2448f760fbef731151353185
bhavna3 <bhavanamothkur2003@gmail.com>
2025-10-26 11:22:46 +0530

    Time: 3 ms (39.66%) | Memory: 18 MB (15.71%) - LeetSync

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -2,13 +2,19 @@ class Solution:
     def totalMoney(self, n: int) -> int:
         summ=0
         m=1
-        day=0
+        day=1
         
         for i in range(1,n+1):
             summ+=m+(i-1)%7
             day+=1
-            if day==7:
+            if day==8:
                 m+=1
-                day=0
+                day=1
 
         return summ   
+
+        
+        #bruteforce
+        #TC O(n)
+        #SC O(1)
+
